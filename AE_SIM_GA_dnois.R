537d5023db2e9855fb551863014fbdeeac091e62
Rıza Mahini <46174354+remahini@users.noreply.github.com>
2022-11-27 21:29:10 +0200

    Update AE_SIM_GA_dnois.R

--- AE_SIM_GA_dnois.R
+++ AE_SIM_GA_dnois.R
@@ -1,10 +1,27 @@
-# Deep AE for jointly learning clustering
+# Deep AE for ERP data -------------------------
+
+# Data was prepared applying the temporal concatenating across the conditions
+# for individual subjects and grand averaged.
+
+# Note: the clustering result(labeling) should be fed on this procedure. Use MATLAB code
+# in downloaded pack for clustering initialization of this DNN.
+
+# Cite as:
+# Deep Clustering Analysis for Time Window Determination of Event-Related Potential
+# January 2022SSRN Electronic Journal
+# DOI: 10.2139/ssrn.4068456
+
+# Copyright:
+# This code provided by Reza Mahini, University of Jyväskylä, Finland.
+# If you had question or comments welcome to send me email to 
+# remahini@jyu.fi
+
 
 # cleaning the workspace
 # rm(list = ls())
 # gc()  #free up memrory and report the memory usage.
 
-# loding the libray --------------------------------------------------
+# Loading the Library -------------------------
 
 library(keras)
 library(ggplot2)
@@ -19,16 +36,18 @@ library(MLmetrics)
 library(aricode)
 
 
-# loading data fro mat file  -----------------------------------------
+# loading data fro mat file  --------------------
 
-inData1 <- readMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/Sim_MS3_data/SimData_Gans.mat") # data without noise
-iData1=as.matrix(inData1$SimData.Gans[,,7]) # selecting dataset for training
+# noise levels : 1=50dB (no_noise) 2=20B, 3=10B, 4=5dB, 5=0dB, 6=-5dB
+
+inData1 <- readMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/Sim_MS3_data/SimDaGA_snr1.mat") # data without noise
+iData1=as.matrix(inData1$SimDaGA.snr1[,,1]) # selecting dataset for training
 
 
-inData <- readMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/Sim_MS3_data/SimData_Gans.mat") # noisy data
-iData=as.matrix(inData$SimData.Gans[,,6]) # selecting dataset for training
-# noise levels : 1=-10dB, 2=-5dB, 3=0dB, 4=5dB, 5=10dB, 6=20dB, 7=50dB
+inData <- readMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/Sim_MS3_data/SimDaGA_snr1.mat") # noisy data
+iData=as.matrix(inData$SimDaGA.snr1[,,2]) # selecting dataset for training
 
+# this labeling is for an assesment 
 Lab <- readMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/CC_GT.mat")
 Lb=as.matrix(Lab$CC.GT) # no noise and ground-truth
 
@@ -40,12 +59,13 @@ nbcl=6 # number of expected class
 d_dim=ncol(iData)
 latent_dim<- c(256, 512)
 BatchSize=150L
+
 # Encoder ----------
 
 input_layer <- layer_input(shape = c(d_dim))
 encoder <- input_layer %>%
   
- 
+  
   layer_dense(units = latent_dim[1], activation = 'tanh', input_shape = c(d_dim)) %>%
   layer_batch_normalization() %>%
   # layer_layer_normalization()%>%
@@ -76,7 +96,7 @@ decoder <-
   layer_dropout(0.2)%>%
   
   
-
+  
   layer_dense(units = latent_dim[1], activation = 'tanh') %>%
   layer_batch_normalization() %>%
   # layer_layer_normalization()%>%
@@ -97,7 +117,7 @@ autoencoder_model %>% compile(
   loss= 'mean_squared_error', # Or try 'categorical_crossentropy', #'mean_squared_error'
   optimizer=optimizer_rmsprop(learning_rate = 0.001), # optimizer_sgd(lr = 0.001) , optimizer_adam(lr = 0.001),optimizer_rmsprop 'sgd', # or try 'rmsprop' ,
   metrics = c('accuracy')
-  )
+)
 
 
 summary(autoencoder_model)
@@ -180,7 +200,10 @@ for(f in unique(folds)){
   tr_inf[f]<-list(matrix(info,nrow = 4,ncol = ep))
   
 }
+
 score <- autoencoder_model%>% keras::evaluate(x_test, x_test)
+score <- autoencoder_model%>% keras::evaluate(x_test, x_test_clean)
+
 
 # we don't need the rest of code (saving the results) --------------------------
 # autoencoder_weights <- 
@@ -214,9 +237,10 @@ cat(" ACC= ", Accuracy(Lb, currentPrediction), ", NMI= ", NMI(Lb, currentPredict
 cat("F1_score= ", F1_Score(currentPrediction,Lb))
 # cat("F1_score= ", confusionMatrix(currentPrediction,Lb))
 
- 
+
 writeMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/dataFeature_AE.mat", dataFeature_AE=intermediate_output)
 writeMat("D:/My works/Current/Deep clustering/SIM_GA_DC_NL/tr_inf_AE.mat", tr_inf.F1=tr_inf[[1]],tr_inf.F2=tr_inf[[2]],
          tr_inf.F3=tr_inf[[3]],tr_inf.F4=tr_inf[[4]],tr_inf.F5=tr_inf[[5]],te_accloss=score)
 
+# End ---------------------
 # End ---------------------
